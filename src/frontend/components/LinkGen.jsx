f302d6824546a5cb34152e5e4f0c5e3430dafde2
Batkhuyag Bavuudorj <Batkhuyag.B@iMac-83.local>
2023-01-18 14:09:09 +0800

    history v1 -fix

--- src/frontend/components/LinkGen.jsx
+++ src/frontend/components/LinkGen.jsx
@@ -64,7 +64,7 @@ export const LinkGen = ({ link }) => {
       <div style={styles.content}>
         <div style={styles.key}>Богино холбоос:</div>
         <div style={styles.copyContainer}>
-          <a style={styles.info} target="_blank" href={`${window.location.href}${data}`}>{window.location.href}{data}</a>
+          <a style={styles.info} target="_blank" rel="noreferrer" href={`${window.location.href}${data}`}>{window.location.href}{data}</a>
           <button style={styles.copyButton} onClick={()=>{navigator.clipboard.writeText(`${window.location.href}${data}`)}}>Хуулж авах</button>
         </div>
       </div>
@@ -134,19 +134,19 @@ function historyObj(data){
   return <div style={styles_History.container}>
     <div style={styles_History.content}>
       <div style={styles_History.key}>Өгөгдсөн холбоос:</div>
-      <a style={styles_History.info} target="_blank" href={data.originalURL} >{data.originalURL}</a>
+      <a style={styles_History.info} target="_blank" rel="noreferrer"  href={data.originalURL} >{data.originalURL}</a>
     </div>
     <div style={styles_History.content}>
       <div style={styles_History.key}>Богино холбоос:</div>
       <div style={styles_History.copyContainer}>
-        <a style={styles_History.info} target="_blank" href={`${window.location.href}${data._id}`}>{window.location.href}{data._id}</a>
+        <a style={styles_History.info} target="_blank" rel="noreferrer" href={`${window.location.href}${data._id}`}>{window.location.href}{data._id}</a>
         <button style={styles_History.copyButton} onClick={()=>{navigator.clipboard.writeText(`${window.location.href}${data._id}`)}}>Хуулж авах</button>
       </div>
     </div>
   </div>
 }
 
-export const LinkHistory = ({}) => {
+export const LinkHistory = () => {
   const [data, setData] = useState()
   useEffect(()=>{
     axios.get("http://localhost:8080").then((response) => {
@@ -158,7 +158,7 @@ export const LinkHistory = ({}) => {
   return (
     <div style={styles_History.main}>
       <h1 style={styles_History.mainText}>Түүх</h1>
-      {data != undefined && data.map((el) => {return historyObj(el)})}
+      {data !== undefined && data.map((el) => {return historyObj(el)})}
     </div>
   );
 };
